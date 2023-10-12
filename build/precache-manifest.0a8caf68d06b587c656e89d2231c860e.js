5d280b3b239ae293027fd2fe38a6576fd00533e7
Prateek Sane <prateeksane@Prateeks-MBP.lan>
2023-10-11 20:14:41 -0700

    change prod folders

---
+++ build/precache-manifest.0a8caf68d06b587c656e89d2231c860e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2842ab268f6c2d79a110155a6d9f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96dcdc5b558d003736d",
+    "url": "/static/css/main.c0beea76.chunk.css"
+  },
+  {
+    "revision": "5ab7e7ce745e1017678e",
+    "url": "/static/js/2.6893b07a.chunk.js"
+  },
+  {
+    "revision": "50b01b64aedfea5f3a01b438bb59e228",
+    "url": "/static/js/2.6893b07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311c76eec6a973e80abd",
+    "url": "/static/js/3.dbac8757.chunk.js"
+  },
+  {
+    "revision": "e96dcdc5b558d003736d",
+    "url": "/static/js/main.18603b5b.chunk.js"
+  },
+  {
+    "revision": "08e82d19684113347fcf",
+    "url": "/static/js/runtime-main.41db00d7.js"
+  }
+]);
